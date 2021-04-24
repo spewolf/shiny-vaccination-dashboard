6a4f700504c45685132e54d8205a0d74ffbfb1d3
Spencer Wolf <spwolf18@gmail.com>
2021-04-24 11:52:54 -0400

    Switch to dynamically loading data from internet

--- app.R
+++ app.R
@@ -1,22 +1,23 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
+# United States Vaccine Rollout Dashboard
+# Spencer Wolf
 #
-# Find out more about building applications with Shiny here:
+# This application allows users to interact with data from the our wold in data
+# github page found here: 
 #
-#    http://shiny.rstudio.com/
+# https://github.com/owid/covid-19-data/blob/master/public/data/vaccinations/us_state_vaccinations.csv
 #
 
 library(shiny)
+library(shinyWidgets)
 library(dplyr)
 library(ggplot2)
 library(plotly)
-library(tidyr)
 
 # Get Data
 state_names = c("Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York State", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming")
 
-data = read.csv('us_state_vaccinations.csv') %>% 
+data = read.csv(url("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/us_state_vaccinations.csv")) %>% 
     mutate(date = as.Date(date, format = "%Y-%m-%d")) %>%
     filter(location %in% state_names)
 states = (data %>% group_by(location) %>% summarise())$location
@@ -32,14 +33,17 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             tags$head(
-                tags$style(HTML('#clear{background-color:red; color:white;}'))
+                tags$style(HTML('#clear{background-color:red; color:white; margin:0 0 0 .4rem;}'))
             ),
             checkboxGroupInput("selected_states",
                                "Select states",
                                choices = state_names,
                                selected= c("Ohio", "Indiana", "New York State", "Alabama", "Alaska")),
-            
-            actionButton("clear", "Clear"),
+            actionGroupButtons(
+                inputIds=c("show_all", "clear"),
+                labels=list("Show All", "Clear All"),
+                direction = "horizontal",
+            ),
             width = 2),
 
         # Show a plot of the generated distribution
@@ -47,26 +51,22 @@ ui <- fluidPage(
            plotlyOutput("vaccinePlot"),
            titlePanel(h2("State Statistics")),
            dataTableOutput("selected_state_stats_dt")
-           # uiOutput("state_stats")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
+    # Actions for selecting states
     observeEvent(input$clear, {
         updateCheckboxGroupInput(session, "selected_states", choices=state_names, selected=NULL)
     })
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    observeEvent(input$show_all, {
+        updateCheckboxGroupInput(session, "selected_states", choices=state_names, selected=state_names)
     })
     
+    # Header plot with full vaccinations per capita
     output$vaccinePlot <- renderPlotly({
         g = data %>% filter(location %in% input$selected_states) %>%
                 drop_na() %>%
@@ -76,18 +76,11 @@ server <- function(input, output, session) {
                 theme(legend.title = element_blank(), legend.position = "", plot.title = element_text(size=10)) +
                 xlab("Date") + 
                 ylab("People Fully Vaccinated per 100 people") +
-                ggtitle("Vaccinations per Capita")
+                ggtitle("Full Vaccinations per Capita")
         ggplotly(g, tooltip = c("text")) %>% layout(legend = list(font = list(size=11)))
     })
     
-    output$state_stats <- renderUI({
-        lapply(input$selected_states, function(i) {
-            fluidRow(
-                h3(i)
-            )
-        })
-    })
-    
+    # Individual state stats datatable
     output$selected_state_stats_dt = renderDataTable({
         data %>% filter(location %in% input$selected_states) %>%
             arrange(location, date) %>% 
